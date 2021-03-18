a24cc315b24efb93059b5b0af4ae816ea2bd9b3d
Jose Ignacio <ignacioalfaro99@gmail.com>
2021-03-18 14:41:04 -0600

    Cambios flipped

--- controllers/othelloController.js
+++ controllers/othelloController.js
@@ -14,10 +14,6 @@ firebase.initializeApp({
 
 
 
-
-
-
-
 function flatten(board) {
     return board.reduce((results, row) => {
         return results.concat(row)
@@ -31,8 +27,87 @@ function boardGenerator() {
     boardMatrix[3][4] = 'O'
     boardMatrix[4][4] = 'X'
     boardMatrix[4][3] = 'O'
+
     return flatten(boardMatrix);
 }
+
+
+
+boardAux = boardGenerator();
+boardAux[29] = 'X';
+boardAux[30]
+
+router.get('/flipped', (req,res) => {
+    console.log( boardAux )
+    flipSquares( boardAux, 21, false );
+    res.status(200).json({ hola: 'hola' })
+});
+
+
+
+
+
+
+function flipSquares(board, position, xIsNext) {
+
+    let modifiedBoard = null;
+    // Calculate row and col of the starting position
+    let [startX, startY] = [position % 8, (position - position % 8) / 8];
+
+    if (board[position] !== null) {
+        
+        return null;
+    }
+
+    // Iterate all directions, these numbers are the offsets in the array to reach next sqaure
+    [1, 7, 8, 9, -1, -7, -8, -9].forEach((offset) => {
+        let flippedSquares = modifiedBoard ? modifiedBoard.slice() : board.slice();
+        let atLeastOneMarkIsFlipped = false;
+        let [lastXpos, lastYPos] = [startX, startY];
+
+        for (let y = position + offset; y < 64; y = y + offset) {
+
+            console.log('Pene')
+            // Calculate the row and col of the current square
+            let [xPos, yPos] = [y % 8, (y - y % 8) / 8];
+
+            // Fix when board is breaking into a new row or col
+            if (Math.abs(lastXpos - xPos) > 1 || Math.abs(lastYPos - yPos) > 1) {
+                break;
+            }
+
+            // Next square was occupied with the opposite color
+            if (flippedSquares[y] === (!xIsNext ? 'X' : 'O')) {
+                flippedSquares[y] = xIsNext ? 'X' : 'O';
+                atLeastOneMarkIsFlipped = true;
+                [lastXpos, lastYPos] = [xPos, yPos];
+                continue;
+            }
+            // Next aquare was occupied with the same color
+            else if ((flippedSquares[y] === (xIsNext ? 'X' : 'O')) && atLeastOneMarkIsFlipped) {
+                flippedSquares[position] = xIsNext ? 'X' : 'O';
+                modifiedBoard = flippedboard.slice();
+            }
+            break;
+        }
+    });
+
+    console.log( modifiedBoard )
+
+    return modifiedBoard;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -60,13 +135,13 @@ router.get('/getGame', async (req, res) => {
         if (idGame) {
             const gameRef = db.collection('games').doc(idGame);
             const docGame = await gameRef.get();
-            if (docGame) {
+            if ( docGame.data() ) {
                 res.status(status.OK).json({ game: docGame.data() });
             } else {
-                res.status(status.INTERNAL_SERVER_ERROR).status({ err: 'Este id del juego es inválido' })
+                res.status(status.INTERNAL_SERVER_ERROR).json({ err: 'Este id del juego es inválido' })
             }
         } else {
-            res.status(status.INTERNAL_SERVER_ERROR).status({ error: 'No se ha enviado el id del juego' })
+            res.status(status.INTERNAL_SERVER_ERROR).json({ error: 'No se ha enviado el id del juego' })
         }
     } catch (err) {
         res.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
@@ -93,9 +168,9 @@ router.put('/editGame', async (req, res) => {
     } catch (err) {
         res.status(status.INTERNAL_SERVER_ERROR).json({ error: err })
     }
-
-
 })
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const http = require('http');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 const hostname = process.env.HOST || 'localhost';
 
 
