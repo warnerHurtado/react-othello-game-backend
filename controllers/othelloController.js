5da9986277aebf4454bcf4c314cd0c4148469cdf
Ignacio <jalfaros@estudiantec.cr>
2021-03-18 16:27:44 -0600

    Cambios
    Se agregó la parte de las reglas del juego

--- controllers/othelloController.js
+++ controllers/othelloController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const firebase = require('firebase-admin')
 const status = require('http-status');
-const serviceAccount = require('../othello-game-2c179-firebase-adminsdk-xbgg6-a9d71ab2a9');
+const serviceAccount = require('../othello-game-2c179-firebase-adminsdk-xbgg6-2318d0fd23.json');
 
 
 
@@ -31,84 +31,14 @@ function boardGenerator() {
     return flatten(boardMatrix);
 }
 
+let boardAux = boardGenerator();
 
 
-boardAux = boardGenerator();
-boardAux[29] = 'X';
-boardAux[30]
-
-router.get('/flipped', (req,res) => {
-    console.log( boardAux )
-    flipSquares( boardAux, 21, false );
+router.get('/flipped', (req, res) => {
+    console.log(boardAux)
+    flipSquares(boardAux, 30, false);
     res.status(200).json({ hola: 'hola' })
 });
-
-
-
-
-
-
-function flipSquares(board, position, xIsNext) {
-
-    let modifiedBoard = null;
-    // Calculate row and col of the starting position
-    let [startX, startY] = [position % 8, (position - position % 8) / 8];
-
-    if (board[position] !== null) {
-        
-        return null;
-    }
-
-    // Iterate all directions, these numbers are the offsets in the array to reach next sqaure
-    [1, 7, 8, 9, -1, -7, -8, -9].forEach((offset) => {
-        let flippedSquares = modifiedBoard ? modifiedBoard.slice() : board.slice();
-        let atLeastOneMarkIsFlipped = false;
-        let [lastXpos, lastYPos] = [startX, startY];
-
-        for (let y = position + offset; y < 64; y = y + offset) {
-
-            console.log('Pene')
-            // Calculate the row and col of the current square
-            let [xPos, yPos] = [y % 8, (y - y % 8) / 8];
-
-            // Fix when board is breaking into a new row or col
-            if (Math.abs(lastXpos - xPos) > 1 || Math.abs(lastYPos - yPos) > 1) {
-                break;
-            }
-
-            // Next square was occupied with the opposite color
-            if (flippedSquares[y] === (!xIsNext ? 'X' : 'O')) {
-                flippedSquares[y] = xIsNext ? 'X' : 'O';
-                atLeastOneMarkIsFlipped = true;
-                [lastXpos, lastYPos] = [xPos, yPos];
-                continue;
-            }
-            // Next aquare was occupied with the same color
-            else if ((flippedSquares[y] === (xIsNext ? 'X' : 'O')) && atLeastOneMarkIsFlipped) {
-                flippedSquares[position] = xIsNext ? 'X' : 'O';
-                modifiedBoard = flippedboard.slice();
-            }
-            break;
-        }
-    });
-
-    console.log( modifiedBoard )
-
-    return modifiedBoard;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 router.get('/newGame', (req, res) => {
@@ -119,6 +49,7 @@ router.get('/newGame', (req, res) => {
         boardGame: boardGenerator(),
         xPlay: true
     }).then(response => {
+        
         res.status(status.OK).json({ idGame: response.id });
     }).catch(err => {
         res.status(status.INTERNAL_SERVER_ERROR).json({ error: err });
@@ -132,10 +63,13 @@ router.get('/getGame', async (req, res) => {
         var db = firebase.firestore();
         const idGame = req.query.idGame;
 
-        if (idGame) {
+        if ( idGame ) {
             const gameRef = db.collection('games').doc(idGame);
             const docGame = await gameRef.get();
-            if ( docGame.data() ) {
+
+            console.log( 'Pafdasdasasd' )
+
+            if (docGame.data()) {
                 res.status(status.OK).json({ game: docGame.data() });
             } else {
                 res.status(status.INTERNAL_SERVER_ERROR).json({ err: 'Este id del juego es inválido' })
@@ -149,26 +83,93 @@ router.get('/getGame', async (req, res) => {
 });
 
 
-router.put('/editGame', async (req, res) => {
+router.post('/editGame', async (req, res) => {
+
 
     const idGame = req.body.idGame;
-    const boardGame = req.body.boardGame;
-    const xPlay = req.body.xPlay;
+    const boardGame = JSON.parse(req.body.boardGame);
+    const xPlay = JSON.parse(req.body.xPlay.toLowerCase());
+    const clickedPosition = parseInt( req.body.clickedPosition );
+    
+    
+    let modifiedBoard = flipSquares(boardGame, clickedPosition, xPlay);
 
-    try {
-        var db = firebase.firestore();
-        db.collection('games').doc(idGame).update({
-            boardGame: JSON.parse(boardGame),
-            xPlay: JSON.parse(xPlay.toLowerCase())
-        }).then(response => {
-            res.status(status.OK).json({ res: response });
-        }).catch(err => {
-            res.status(status.INTERNAL_SERVER_ERROR).json({ error: err })
-        });
-    } catch (err) {
-        res.status(status.INTERNAL_SERVER_ERROR).json({ error: err })
+   
+
+    if ( modifiedBoard !== null ) {
+        try {
+
+            var pool = firebase.firestore();
+            await pool.collection('games').doc( idGame ).update({
+                boardGame   : modifiedBoard,
+                xPlay       : xPlay 
+            
+            }).then( () => {
+                res.status(200).json( { success : 200 } );
+            }).catch( () => {
+                res.status(500).json( { error : err} );
+            })
+
+        } catch ( err ) {
+            res.status( status.INTERNAL_SERVER_ERROR ).json( { error: err  } );
+        }
+
+    } else {
+        res.status(500).json({ success: 500 })
     }
-})
+
+
+});
+
+
+
+
+
+
+function flipSquares(board, position, xIsNext) {
+
+    let modifiedBoard = null;
+    let [startX, startY] = [position % 8, (position - position % 8) / 8];
+
+    console.log( board, position, xIsNext )
+
+    if (board[position] !== null) {
+        return null;
+    }
+
+    [1, 7, 8, 9, -1, -7, -8, -9].forEach((offset) => {
+        let flippedSquares = modifiedBoard ? modifiedBoard.slice() : board.slice();
+        let atLeastOneMarkIsFlipped = false;
+        let [lastXpos, lastYPos] = [startX, startY];
+
+        for (let y = position + offset; y < 64; y = y + offset) {
+
+            let [xPos, yPos] = [y % 8, (y - y % 8) / 8];
+
+            if (Math.abs(lastXpos - xPos) > 1 || Math.abs(lastYPos - yPos) > 1) {
+                break;
+            }
+            if (flippedSquares[y] === (!xIsNext ? 'X' : 'O')) {
+
+                flippedSquares[y] = xIsNext ? 'X' : 'O';
+                atLeastOneMarkIsFlipped = true;
+                [lastXpos, lastYPos] = [xPos, yPos];
+                continue;
+            }
+            // Next aquare was occupied with the same color
+            else if ((flippedSquares[y] === (xIsNext ? 'X' : 'O')) && atLeastOneMarkIsFlipped) {
+                flippedSquares[position] = xIsNext ? 'X' : 'O';
+                console.log('hola else if')
+                modifiedBoard = flippedSquares.slice();
+            }
+            break;
+        }
+    });
+
+    console.log( modifiedBoard )
+    return modifiedBoard;
+}
+
 
 
 
